22dd793a1de763b12f9a1e2e3e7a18172487419a
ChiaoYunTing <you@example.com>
2024-03-16 18:51:00 +0000

    update data and R file

--- R/load_data.R
+++ R/load_data.R
@@ -1,26 +1,133 @@
+library(ggplot2)
+library(dplyr)
 library(readr)
 library(RSQLite)
+library(DBI)
 
-# Load the datasets
-category <- readr::read_csv("dataset/Category_v2.csv")
-customer <- readr::read_csv("dataset/customer_v2.csv")
-newproduct <- readr::read_csv("dataset/new_product.csv")
-order1 <- readr::read_csv("dataset/order_v4.csv")
-product <- readr::read_csv("dataset/product_v2.csv")
-shipping <- readr::read_csv("dataset/shipping_v3.csv")
-supplier <- readr::read_csv("dataset/supplier_v2.csv")
+# Load the original datasets
+category <- readr::read_csv("dataset/Category/Category_v2.csv")
+customer <- readr::read_csv("dataset/Customer/customer_v2.csv")
+order <- readr::read_csv("dataset/Order/order_v4.csv")
+product <- readr::read_csv("dataset/Product/product_v2.csv")
+shipping <- readr::read_csv("dataset/Shipping/shipping_v3.csv")
+supplier <- readr::read_csv("dataset/Supplier/supplier_v2.csv")
+
+#change date data to character 
+customer$Date_of_birth <- as.character(customer$Date_of_birth)
+order$Order_date <- as.character(order$Order_date)
+shipping$Shipping_date <- as.character(shipping$Shipping_date)
 
 # Connect to the database
-my_connection <- RSQLite::dbConnect(RSQLite::SQLite(), "database/group_33.db")
+database <- dbConnect(RSQLite::SQLite(), dbname = "group_33.db")
+
+
+# If CUSTOMER table exist, drop it
+if(dbExistsTable(database, "CUSTOMER")){
+  dbExecute(database, "DROP TABLE CUSTOMER")
+}
+
+#create CUSTOMER table
+dbExecute(database, "CREATE TABLE 'CUSTOMER' (
+                'Customer_id' CHAR(5) PRIMARY KEY,
+                'First_name' VARCHAR NOT NULL,
+                'Last_name' VARCHAR NOT NULL,
+                'Gender' VARCHAR NOT NULL,
+                'Date_of_birth' DATE NOT NULL,
+                'Email' VARCHAR NOT NULL UNIQUE,
+                'Phone_number' VARCHAR NOT NULL UNIQUE)")
+
+# If ORDER table exist, drop it
+if(dbExistsTable(database, "ORDER")){
+  dbExecute(database, "DROP TABLE 'ORDER'")
+}
+
+#create ORDER table
+dbExecute(database, "CREATE TABLE 'ORDER' (
+                'Order_id' CHAR(6) NOT NULL,
+                'Customer_id' CHAR(5) NOT NULL,
+                'Product_id' CHAR(6) NOT NULL,
+                'Shipping_id' CHAR(6) NOT NULL,
+                'Order_date' DATE NOT NULL,
+                'Quantity' INT NOT NULL,
+                'Promotion_description' VARCHAR,
+                'Discount_percent' DECIMAL, 
+                'Card_number' VARCHAR(16) NOT NULL,
+                'Card_scheme' VARCHAR NOT NULL,
+                'Card_type' VARCHAR NOT NULL,
+                'Security_code' CHAR(3) NOT NULL,
+                'Cardholder_name' VARCHAR NOT NULL,
+                'YY' CHAR(2) NOT NULL,
+                'MM' CHAR(2) NOT NULL,
+                PRIMARY KEY ('Order_id', 'Customer_id', 'Product_id', 'Shipping_id'),
+                FOREIGN KEY ('Customer_id') REFERENCES CUSTOMER ('Customer_id'),
+                FOREIGN KEY ('Product_id') REFERENCES PRODUCT ('Product_id'),
+                FOREIGN KEY ('Shipping_id') REFERENCES SHIPPING ('Shipping_id'))")
+
+# If SHIPPING table exist, drop it
+if(dbExistsTable(database, "SHIPPING")){
+  dbExecute(database, "DROP TABLE SHIPPING")
+}
+
+#create SHIPPING table
+dbExecute(database, "CREATE TABLE 'SHIPPING' (
+                'Shipping_id' CHAR(6) PRIMARY KEY,
+                'Shipper_name' VARCHAR NOT NULL,
+                'Shipping_status' VARCHAR NOT NULL,
+                'Shipping_date' DATE NOT NULL,
+                'Address_line_1' VARCHAR NOT NULL,
+                'Postcode' VARCHAR NOT NULL,
+                'City' VARCHAR NOT NULL,
+                'Country' VARCHAR NOT NULL)")
+
+# If PRODUCT table exist, drop it
+if(dbExistsTable(database, "PRODUCT")){
+  dbExecute(database, "DROP TABLE PRODUCT")
+}
+
+#create PRODUCT table
+dbExecute(database, "CREATE TABLE 'PRODUCT' (
+                'Product_id' CHAR(6) PRIMARY KEY,
+                'Product_name' VARCHAR NOT NULL,
+                'Product_description' VARCHAR,
+                'Unit_price' DECIMAL NOT NULL,
+                'Stock' INT NOT NULL, 
+                'Supplier_id' CHAR(6) NOT NULL,
+                'Category_id' CHAR(6) NOT NULL,
+                FOREIGN KEY ('Supplier_id') REFERENCES SUPPLIER ('Supplier_id'),
+                FOREIGN KEY ('Category_id') REFERENCES CATEGORY ('Category_id'))")
+
+# If SUPPLIER table exist, drop it
+if(dbExistsTable(database, "SUPPLIER")){
+  dbExecute(database, "DROP TABLE SUPPLIER")
+}
+
+#create SUPPLIER table
+dbExecute(database, "CREATE TABLE 'SUPPLIER' (
+                'Supplier_id' CHAR(6) PRIMARY KEY,
+                'Supplier_phone' VARCHAR NOT NULL,
+                'Supplier_name' VARCHAR NOT NULL)")
+
+# If CATEGORY table exist, drop it
+if(dbExistsTable(database, "CATEGORY")){
+  dbExecute(database, "DROP TABLE CATEGORY")
+}
+dbExecute(database, "CREATE TABLE 'CATEGORY' (
+                'Category_id' CHAR(6) PRIMARY KEY,
+                'Category_name' VARCHAR NOT NULL,
+                'Parent_category' VARCHAR,
+                'Parent_category_id' CHAR(6))")
+
 
 # Append each data frame to its respective table in the database
 # Adjust the table names as needed
-RSQLite::dbWriteTable(my_connection, "category", category, append = TRUE)
-RSQLite::dbWriteTable(my_connection, "customer", customer, append = TRUE)
-RSQLite::dbWriteTable(my_connection, "new_product", newproduct, append = TRUE)
-RSQLite::dbWriteTable(my_connection, "order", order1, append = TRUE)
-RSQLite::dbWriteTable(my_connection, "product", product, append = TRUE)
-RSQLite::dbWriteTable(my_connection, "shipping", shipping, append = TRUE)
-RSQLite::dbWriteTable(my_connection, "supplier", supplier, append = TRUE)
+dbWriteTable(database, "CUSTOMER", customer, append = TRUE)
+dbWriteTable(database, "ORDER", order, append = TRUE)
+dbWriteTable(database, "SHIPPING", shipping, append = TRUE)
+dbWriteTable(database, "PRODUCT", product, append = TRUE)
+dbWriteTable(database, "SUPPLIER", supplier, append = TRUE)
+dbWriteTable(database, "CATEGORY", category, append = TRUE)
 
-#read new data
+
+# Disconnect from the database
+RSQLite::dbDisconnect(database)
+
---
+++ R/update.R
@@ -0,0 +1,56 @@
+library(dplyr)
+library(readr)
+library(RSQLite)
+library(DBI)
+
+# Connect to the database
+database <- dbConnect(RSQLite::SQLite(), dbname = "group_33.db")
+
+#read new data
+# Function to append new files to database
+append_new_data <- function(directory, table_name, connection) {
+  # Define the path for the log of processed files
+  log_file_path <- paste0(directory, "/processed_files.log")
+  
+  # Read the log file to get a list of processed files, if it exists
+  if (file.exists(log_file_path)) {
+    processed_files <- readLines(log_file_path)
+  } else {
+    processed_files <- character(0)
+  }
+  
+  # List all CSV files in the directory
+  all_files <- list.files(directory, full.names = TRUE, pattern = "\\.csv$")
+  
+  # Update log to only include existing files
+  processed_files <- processed_files[processed_files %in% all_files]
+  
+  # Determine new files by excluding processed files
+  new_files <- setdiff(all_files, processed_files)
+  
+  # Process each new file
+  for (file in new_files) {
+    # Read data from the new file
+    new_data <- readr::read_csv(file)
+    
+    # Append data to the database
+    RSQLite::dbWriteTable(connection, table_name, new_data, append = TRUE, overwrite=FALSE)
+  }
+  
+  # Update the log file with the current list of processed files
+  writeLines(c(processed_files, new_files), log_file_path)
+}
+
+# Define the list of directories and corresponding table names
+directories <- c("Category", "Customer", "Order", "Product", "Shipping", "Supplier")
+table_names <- c("CATEGORY", "CUSTOMER", "ORDER", "PRODUCT", "SHIPPING", "SUPPLIER")
+
+# Iterate over each directory and append new data
+for (i in seq_along(directories)) {
+  append_new_data(paste0("dataset/", directories[i]), table_names[i], database)
+}
+
+dbGetQuery(database, "SELECT * FROM PRODUCT")
+
+# Disconnect from the database
+RSQLite::dbDisconnect(database)
