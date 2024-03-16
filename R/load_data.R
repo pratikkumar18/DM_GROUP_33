993dbface8deabba722e9dede05dda91692ca4ee
ChiaoYunTing <you@example.com>
2024-03-16 20:37:25 +0000

    update R file

--- R/load_data.R
+++ R/load_data.R
@@ -127,8 +127,128 @@ dbWriteTable(database, "PRODUCT", product, append = TRUE)
 dbWriteTable(database, "SUPPLIER", supplier, append = TRUE)
 dbWriteTable(database, "CATEGORY", category, append = TRUE)
 
+
+#update data
+#read new data
+
+# Function to append new files to database
+append_new_data <- function(directory, table_name, connection) {
+  # Define the path for the log of processed files
+  log_file_path <- paste0(directory, "/processed_files.log")
+  
+  # Read the log file to get a list of processed files, if it exists
+  if (file.exists(log_file_path)) {
+    processed_files <- readLines(log_file_path)
+  } else {
+    processed_files <- character(0)
+  }
+  
+  # List all CSV files in the directory
+  all_files <- list.files(directory, full.names = TRUE, pattern = "\\.csv$")
+  
+  # Update log to only include existing files
+  processed_files <- processed_files[processed_files %in% all_files]
+  
+  # Determine new files by excluding processed files
+  new_files <- setdiff(all_files, processed_files)
+  
+  # Process each new file
+  for (file in new_files) {
+    # Read data from the new file
+    new_data <- readr::read_csv(file)
+    
+    # Append data to the database
+    RSQLite::dbWriteTable(connection, table_name, new_data, append = TRUE, overwrite=FALSE)
+  }
+  
+  # Update the log file with the current list of processed files
+  writeLines(c(processed_files, new_files), log_file_path)
+}
+
+# Define the list of directories and corresponding table names
+directories <- c("Category", "Customer", "Order", "Product", "Shipping", "Supplier")
+table_names <- c("CATEGORY", "CUSTOMER", "ORDER", "PRODUCT", "SHIPPING", "SUPPLIER")
+
+# Iterate over each directory and append new data
+for (i in seq_along(directories)) {
+  append_new_data(paste0("dataset/", directories[i]), table_names[i], database)
+}
+
+dbGetQuery(database, "SELECT * FROM PRODUCT")
+
+#plot graph
+# Proportion of customers by gender
+customer_data <- customer %>%
+  group_by(Gender) %>%
+  count() %>%
+  ungroup() %>%
+  mutate(percentage = `n` / sum(`n`)) %>%
+  mutate(percent_label = scales::percent(percentage))
+
+plot1 <- ggplot(customer_data, aes(x = "", y = percentage, fill = Gender)) +
+  geom_col(color = "black") +
+  geom_label(aes(label = percent_label), color = c(1, 1),
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) +
+  guides(fill = guide_legend(title = "Gender")) +
+  coord_polar(theta = "y") +
+  labs(title = "Proportion of Customers by Gender") +
+  theme_void()
+
+#save the plot to "figures" directory
+ggsave("figures/customer_gender_proportion.png", plot = plot1)
+
+#Top 10 Customers with the Highest Purchase Frequency
+
+#connect to database
+database <- dbConnect(RSQLite::SQLite(), dbname = "group_33.db")
+
+top_purchased_customer <- RSQLite::dbGetQuery(database, "SELECT c.Customer_id, c.First_name, c.Last_name, oc.Order_Count
+FROM (
+    SELECT Customer_id, COUNT(DISTINCT Order_id) AS Order_Count
+    FROM 'ORDER'
+    GROUP BY Customer_id
+    ORDER BY COUNT(DISTINCT Order_id) DESC
+    LIMIT 10
+) AS oc
+JOIN CUSTOMER c ON oc.Customer_id = c.customer_id;")
+
+plot2 <- ggplot(top_purchased_customer, aes(x=reorder(Customer_id, Order_Count), y=Order_Count)) +
+  geom_bar(stat="identity", fill="skyblue") +
+  coord_flip() + # Flips the axes
+  labs(title="Top 10 Customers with the Highest Purchase Frequency", x="Customer ID", y="Total Purchase Times") +
+  theme_minimal()
+
+#save the plot to "figures" directory
+ggsave("figures/Top10_customer_with_highest_purchase_frequency.png", plot = plot2)
+
+# Top 10 Customers with the Highest Order Value
+
+top_order_value_customer <- RSQLite::dbGetQuery(database, "SELECT
+    c.Customer_id,
+    SUM((o.Quantity * p.Unit_price) * (1 - COALESCE(o.Discount_percent, 0))) AS Total_Order_Value
+FROM
+    CUSTOMER c
+JOIN
+    'ORDER' o ON c.Customer_id = o.Customer_id
+JOIN
+    PRODUCT p ON o.Product_id = p.Product_id
+GROUP BY
+    c.Customer_id, c.First_name, c.Last_name
+ORDER BY
+    Total_Order_Value DESC
+LIMIT 10;")
+
+plot3 <- ggplot(top_order_value_customer, aes(x=reorder(Customer_id, Total_Order_Value), y=Total_Order_Value)) +
+  geom_bar(stat="identity", fill="skyblue") +
+  coord_flip() + # Flips the axes
+  labs(title="Top 10 Customers with the Highest Order Value", x="Customer ID", y="Total Order Value") +
+  theme_minimal()
+
+#save the plot to "figures" directory
+ggsave("figures/Top10_Customers_with_the_highest_order_value.png", plot = plot3)
+
+
 # Disconnect from the database
 RSQLite::dbDisconnect(database)
-
-save.image("my_workspace.RData")
 
